9b30c74b76875f1d13a410ee60aba3fd729fe803
UsmanButt <buttu7666@gmail.com>
2024-12-04 02:55:45 +0500

    Sound added and ball logic updated
    - Ball doesn't rotate now
    - Ball's vertical velocity is recalculated on every paddle hit
    - Sound effects added for wall and paddle hits

--- Pong!/Assets/Scripts/Ball.cs
+++ Pong!/Assets/Scripts/Ball.cs
@@ -2,26 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
-{
+public class Ball : MonoBehaviour {
     public Rigidbody2D rb;
     public float startingSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        bool isRight = UnityEngine.Random.value >= 5;
+
+    // Audio variables
+    public AudioClip wallHitSound;
+    public AudioClip paddleHitSound;
+    private AudioSource audioSource;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();  // Get the AudioSource component on the ball
+        bool isRight = UnityEngine.Random.value >= 0.5f;
         float xVelocity = -1f;
-        if(isRight == true) {
+
+        if (isRight) {
             xVelocity = 1f;
         }
 
-        float yVelocity = UnityEngine.Random.Range(1, -1);
+        float yVelocity = UnityEngine.Random.Range(-1.5f, 1.5f);
         rb.velocity = new Vector2(xVelocity * startingSpeed, yVelocity * startingSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Wall")) {
+            audioSource.PlayOneShot(wallHitSound);
+        }
+        if (collision.gameObject.CompareTag("Paddle")) {
+            audioSource.PlayOneShot(paddleHitSound);
+            float yVelocity = UnityEngine.Random.Range(-1.5f, 1.5f);
+            rb.velocity = new Vector2(rb.velocity.x, yVelocity * startingSpeed);
+        }
     }
 }
